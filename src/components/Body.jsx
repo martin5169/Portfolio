23430a973ea79f7ed7151a035ab2a7b616d0df3b
martin5169 <martincommisso@hotmail.com>
2023-12-07 17:31:21 -0300

    img fixed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -34,7 +34,7 @@ function Body() {
         </Navbar.Brand>
       </Container>
       <div className="imagen">
-        <img src="/public/assets/pc.png" alt="pc" />
+        <img src="/public/pc.png" alt="pc" />
       </div>
 
       <div className="aboutMe">
