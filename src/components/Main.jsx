cbeb450a8df02501f20743e1389f96e2383ca3d6
martin5169 <martincommisso@hotmail.com>
2024-01-31 16:09:15 -0300

    card change

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,7 @@ import store from '../redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Provider store={store}>
-    
+
   <Navigation />
   <Body/>
   <Cards/>
